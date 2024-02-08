99eb2759db57a438579690aa05a750976761bdc4
root <root@siia-teste-05.aman.eb.mil.br>
2024-02-08 15:09:04 -0300

    Adicionar Icone a ao layout de usuários não conectados

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" type="image/png" href="{{ asset('assets/icons/connectdevelop.svg') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
