0fd6f54c0363cda6d9eb34ed63dcaa52b78b0bb4
root <root@siia-teste-05.aman.eb.mil.br>
2024-02-16 11:54:55 -0300

    Atualizar component Footer da aplicação

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-gray-800 py-4">
     <div class="container mx-auto text-center text-gray-400 text-sm">
         <p class="mb-2">&copy; 2024 - Todos os direitos reservados | Versão: <b>1.0</b></p>
-        <p>Desenvolvido por: <b>Danilo de Andrade Ferreira Sousa</b></p>
+        <p>Desenvolvido por: <a href="https://www.instagram.com/danilodevweb" target="_blank"><b>Danilo de Andrade Ferreira Sousa</b></a></p>
     </div>
 </footer>
